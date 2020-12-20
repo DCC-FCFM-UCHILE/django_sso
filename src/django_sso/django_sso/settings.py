d9924a555fca3ce7554401a4830283e52d57bf12
Juan Arriagada <jarrcan@gmail.com>
2020-12-20 20:53:44 -0300

    para pruebas en test

--- src/django_sso/django_sso/settings.py
+++ src/django_sso/django_sso/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "django_sso.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates"), os.path.join(BASE_DIR, "apps")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
