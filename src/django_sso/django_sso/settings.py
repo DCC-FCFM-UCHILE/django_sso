a2f9cb8a7b5eaec665e4bde577d383e7a8c0a859
Juan Arriagada <jarrcan@gmail.com>
2020-12-20 13:20:21 -0300

    version para pruebas

--- src/django_sso/django_sso/settings.py
+++ src/django_sso/django_sso/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "sso.apps.SsoConfig",
+    "demo.apps.DemoConfig",
 ]
 
 MIDDLEWARE = [
